85d293879b505d8e847d800aa1a2bc737813b4c7
Deepak-S-a-h-u <115860022+Deepak-S-a-h-u@users.noreply.github.com>
2022-12-30 19:58:02 +0530

    added by dropping

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Routing from './Screens/Routing';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Routing/>
+     {/* <Route path='/routing' element={<Routing/>}/> */}
+
+    </div>
+  );
+}
+
+export default App;
